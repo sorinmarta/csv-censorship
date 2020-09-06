3ec018940afa10a30e44eb8057454a4f5b4558f3
Sorin Marta <sorinmarta@Sorins-MacBook-Pro-2.local>
2020-09-06 20:06:48 +0300

    Added comments and moved the required values to the top of the file

--- index.php
+++ index.php
@@ -1,7 +1,27 @@
 <?php
 
+/**
+ * This is a tool that's going to take a CSV file and will manipulate a certain column on
+ * every row and will censor the contents with asterix
+ *
+ * There is a minimum value also included so if you would like to have some columns not
+ * censored because the censorship won't make sense. You can set a minimum length.
+ *
+ * If you don't want to do that, you can simply set the $minLength to 0
+ */
 
+/**
+ * Required values
+ */
+
+// Name of the initial file
 $filename = 'initialFile.csv';
+// Name of the file that will be exported
+$updatedFileRaw = 'updated.csv';
+// Minimum length of a column contains
+$minLength = 14;
+// Name of the default value for less than minimum entries
+$defaultValue = '**...**';
 
 $the_big_array = []; 
 
@@ -21,8 +41,8 @@ if (($h = fopen("{$filename}", "r")) !== FALSE)
 		$asterixLen = $stringLen - 8;
 		$element = array();
 
-		if ($stringLen > 14) {
-			$asterix = "**...**";
+		if ($stringLen > $minLength) {
+			$asterix = $defaultValue;
 		}else{
 			for ($i=0; $i <= $asterixLen; $i++) { 
 				array_push($element, '*');
@@ -35,7 +55,7 @@ if (($h = fopen("{$filename}", "r")) !== FALSE)
 		array_push($editedValues, $replace);
 	}
 
-	$updatedFile = fopen('updated.csv', 'w');
+	$updatedFile = fopen($updatedFileRaw, 'w');
 
 	for ($i=0; $i <= $rowsNumber; $i++) { 
 			$rows[$i][1] = $editedValues[$i];
